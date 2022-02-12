094e18edb6ba1d4d4e12252529bf98771ad4766f
juancoquet <juancoquet@gmail.com>
2022-02-12 14:47:00 +0000

    solve 2.2 'kth-to-last'

--- 02_linked_lists.py
+++ 02_linked_lists.py
@@ -11,6 +11,8 @@ llist.add(1)
 llist.add(6)
 llist.add(4)
 
+
+# 2.1 remove duplicates
 def remove_dups(linked_list):
     exists = {}
     current = linked_list.head
@@ -25,3 +27,44 @@ def remove_dups(linked_list):
             current.next = None
             current = previous.next
 
+
+# 2.2 kth to last
+def size(linked_list):
+	count = 0
+	i = 0
+	indexed = {}
+	current = linked_list.head
+	while current is not None:
+		count += 1
+		indexed[i] = current
+		i += 1
+		current = current.next
+	return count, indexed
+
+def get_kth_to_last(linked_list, k):
+	count, indexed = size(linked_list)
+	if k > count:
+		raise IndexError("Index out of range")
+	if k < 1:
+		raise IndexError("Index must be greater than 0")
+	i = count - k
+	return indexed[i]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for item in llist:
+    print(item, end=", ")
