afa30c21df5284d104717e8cf409fdce9e93497e
juancoquet <juancoquet@gmail.com>
2022-04-14 12:41:33 +0100

    solve 10.3 search in rotated sorted arary

--- 10_sorting_and_searching.py
+++ 10_sorting_and_searching.py
@@ -35,5 +35,30 @@ def is_anagram(s1, s2):
         return False
     return True
 
-unordered_anagrams = ['abc', 'def', 'ghi', 'cab', 'gef', 'hig']
-print(group_anagrams(unordered_anagrams))
+# solving 10.3 search in rotated sorted array
+def search_rotated_array(a, target):
+    splitpoint = 0
+    for i in range(1, len(a)):
+        if a[i] < a[i-1]:
+            splitpoint = i
+            break
+    b = a[splitpoint:] + a[:splitpoint]
+    target_found = bin_search(target, b)
+    if target_found is not None:
+        return (target_found + splitpoint) % len(a)
+    else:
+        return None
+
+def bin_search(target, a, left_i=0):
+    if len(a) == 0:
+        return None
+    midpoint = len(a) // 2
+    if a[midpoint] == target:
+        return midpoint + left_i
+    if a[midpoint] < target:
+        return bin_search(target, a[midpoint+1:], left_i=midpoint+1)
+    else:
+        return bin_search(target, a[:midpoint], left_i=left_i)
+
+
+a = [15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14]
