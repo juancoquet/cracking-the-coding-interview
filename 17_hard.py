dbafbd36362fc32a041a53821a19c1a31be03919
juancoquet <juancoquet@gmail.com>
2022-04-15 11:33:44 +0100

    solve 17.2 shuffle

--- 17_hard.py
+++ 17_hard.py
@@ -1,30 +1,40 @@
 # 17.1 Add without plus
 def add_without_plus(a, b):
-	bit_length = max(a.bit_length(), b.bit_length())
-	carry = False
-	result = 0
-	for i in range(bit_length + 1):
-		a_bit = get_bit(a, i)
-		b_bit = get_bit(b, i)
-		added = a_bit ^ b_bit
-		if carry:
-			added = added ^ 1
-		if (a_bit and b_bit) or ((a_bit != b_bit) and carry):
-			carry = True
-		else:
-			carry = False
-		if added == 1:
-			result = set_bit(result, i)
-	return result
-		
+    bit_length = max(a.bit_length(), b.bit_length())
+    carry = False
+    result = 0
+    for i in range(bit_length + 1):
+        a_bit = get_bit(a, i)
+        b_bit = get_bit(b, i)
+        added = a_bit ^ b_bit
+        if carry:
+            added = added ^ 1
+        if (a_bit and b_bit) or ((a_bit != b_bit) and carry):
+            carry = True
+        else:
+            carry = False
+        if added == 1:
+            result = set_bit(result, i)
+    return result
+        
 
 def get_bit(num, i):
-	mask = 1 << i
-	if num & mask != 0:
-		return 1
-	else:
-		return 0
+    mask = 1 << i
+    if num & mask != 0:
+        return 1
+    else:
+        return 0
 
 def set_bit(num, i):
-	return num | (1 << i)
+    return num | (1 << i)
 
+# 17.2 shuffle
+import random
+
+def shuffle(deck):
+    shuffled = []
+    while len(deck) > 0:
+        i = random.randrange(0, len(deck))
+        card = deck.pop(i)
+        shuffled.append(card)
+    return shuffled
