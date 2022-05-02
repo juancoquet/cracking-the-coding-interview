f2933b7f71c9804eb2238420e781800c125eb0cd
juancoquet <juancoquet@gmail.com>
2022-05-02 22:12:48 +0100

    solve 16.6 'smallest difference'

--- 16_moderate.py
+++ 16_moderate.py
@@ -47,17 +47,65 @@ def tic_tac_win(board, board_size=3):
 
 # 16.5 factorial zeros
 def factorial(n):
-	if n == 1:
-		return 1
-	return factorial(n-1) * n
+    if n == 1:
+        return 1
+    return factorial(n-1) * n
 
 def trailing_zeros(n):
-	zeros = 0
-	while n % 10 == 0:
-		zeros += 1
-		n /= 10
+    zeros = 0
+    while n % 10 == 0:
+        zeros += 1
+        n /= 10
 
 def factorial_zeros(n):
-	fact = factorial(n)
-	zeros = trailing_zeros(fact)
-	return zeros
+    fact = factorial(n)
+    zeros = trailing_zeros(fact)
+    return zeros
+
+
+# 16.6 smallest difference
+def _smallest_difference(arr, midpoint=None):
+    if len(arr) > 1:
+        if midpoint is None:
+            midpoint = len(arr) // 2
+        left = arr[:midpoint]
+        right = arr[midpoint:]
+        
+        _smallest_difference(left)
+        _smallest_difference(right)
+        
+        li, ri, i = 0, 0, 0
+        min_diff = float('inf')
+    
+        while li < len(left) and ri < len(right):
+            if left[li] <= right[ri]:
+                arr[i] = left[li]
+                li += 1
+            elif left[li] > right[ri]:
+                arr[i] = right[ri]
+                if left[li] - right[ri] < min_diff:
+                    min_diff = left[li] - right[ri]
+                ri += 1
+            i += 1
+    
+        while li < len(left):
+            arr[i] = left[li]
+            if left[li] - right[-1] < min_diff:
+                min_diff = left[li] - right[-1]
+            li += 1
+            i += 1
+    
+        while ri < len(right):
+            arr[i] = right[ri]
+            ri += 1
+            i += 1
+    
+        if min_diff == float('inf'):
+            min_diff = None
+    
+        return min_diff
+
+def smallest_difference(a_arr, b_arr):
+    _smallest_difference(a_arr) # merge sort
+    _smallest_difference(b_arr) # merge sort
+    return _smallest_difference(a_arr + b_arr, midpoint=len(a_arr))
